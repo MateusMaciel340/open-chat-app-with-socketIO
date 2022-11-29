b78bbe8f137e809615993cea8f851b5060a63f16
mateusmaciel340 <mateusmaciel340@gmail.com>
2022-11-29 16:53:29 -0300

    feat: initial setup for errors and socketio

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,49 @@
-import express, { Application } from "express";
+import express, { Application, NextFunction, Request, Response } from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import swaggerUi from "swagger-ui-express";
+
+import AppError from "./errors/AppError";
+import socketio, { Socket, Server } from "socket.io";
 
 import swaggerFile from "./swagger/swagger.json";
 import router from "./routes/route";
 
 const app: Application = express();
+const io = new Server({});
+
+const connectedUsers: any = {};
 
 dotenv.config();
+
+// SocketIO
+io.on("connection", (socket: Socket) => {
+    const { user_id } = socket.handshake.query;
+});
+
+app.use((request: Request, response: Response, next: NextFunction) => {
+    request.io = io;
+    request.connectedUsers = connectedUsers;
+
+    return next();
+})
+
+// Error
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+    if (err instanceof AppError) {
+        return response.status(err.statusCode).json({
+            status: "error",
+            message: err.message
+        });
+    }
+
+    console.error(err);
+
+    return response.status(500).json({
+        status: "error",
+        message: "Internal server error"
+    });
+});
 
 app.use(
     cors(), express.json(), router
